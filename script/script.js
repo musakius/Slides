7e26e6ce200e77432128d8be0020696d8ccdc83c
Petr <musakius.work@gmail.com>
2020-06-27 13:44:54 +0300

    Slides

---
+++ script/script.js
@@ -0,0 +1,132 @@
+const blockLink = document.querySelector(".block-link");
+const blockSearch = document.querySelector(".block-search");
+
+function openCloseBurger() {
+  const burger = document.querySelector(".burger");
+  const blockControlSlider = document.querySelector(".block-control-slider");
+  const footerPanel = document.querySelector(".footer-panel");
+  const container = document.querySelector(".container");
+  const closeBurger = document.querySelector(".close");
+  const menu = document.querySelector(".menu");
+
+  burger.addEventListener("click", toggleBurger);
+  closeBurger.addEventListener("click", toggleBurger);
+
+  function toggleBurger() {
+    burger.classList.toggle("open");
+    footerPanel.classList.toggle("content-center");
+    closeBurger.style.display = "block";
+    blockLink.style.transform = "scale(0) translate(0px, -400px)";
+    blockSearch.style.display = "none";
+    blockControlSlider.style.display = "none";
+    container.style.display = "none";
+    menu.style.display = "flex";
+    document.body.style.backgroundImage = "url('../slide/img/menu.png')";
+
+    if (!burger.classList.contains("open")) {
+      closeBurger.style.display = "none";
+      blockLink.style.transform = "scale(1)";
+      blockSearch.style.display = "block";
+      blockControlSlider.style.display = "block";
+      container.style.display = "block";
+      menu.style.display = "none";
+      document.body.style.backgroundImage = "url('../slide/img/slide_1.png')";
+    }
+  }
+}
+
+///////////////////////////////////////////////////////////
+
+function openCloseSearch() {
+  const loupe = document.querySelector(".fa-search");
+  const closeSearch = document.querySelector(".fa-times");
+  const blockInput = document.querySelector(".block-input");
+  const border = document.querySelector(".border");
+
+  loupe.addEventListener("click", () => {
+    blockInput.style.display = "flex";
+    blockLink.style.display = "none";
+    blockSearch.classList.add("block-search-open");
+    loupe.style.padding = "0 10px";
+    border.style.display = "block";
+  });
+
+  closeSearch.addEventListener("click", () => {
+    blockInput.style.display = "none";
+    blockLink.style.display = "flex";
+    blockSearch.classList.remove("block-search-open");
+    loupe.style.padding = "0";
+    border.style.display = "none";
+  });
+}
+
+/////////////////////////////////////////////////////////
+
+function renderImgInMenu() {
+  const menuBar = document.querySelectorAll(".menu-bar li a");
+  const imgLeft = document.querySelector(".img-left");
+  const imgRight = document.querySelector(".img-right");
+
+  menuBar.forEach((el, i) => {
+    const slideIndex =
+      i < menuBar.length / 2 ? i + 1 : i - menuBar.length / 2 + 1;
+    console.log();
+    el.addEventListener("mouseover", () => {
+      menuBar.forEach((item) => (item.style.color = "#4444445b"));
+      el.style.color = "#407939";
+      if (i % 2 === 0) {
+        imgLeft.style.display = "block";
+        imgLeft.src = `../slide/img/img-hover_${slideIndex}.png`;
+      } else {
+        imgRight.style.display = "block";
+        imgRight.src = `../slide/img/img-hover_${slideIndex}.png`;
+      }
+      imgLeft.style.top = `${slideIndex * 90}px`;
+      imgRight.style.top = `${slideIndex * 90}px`;
+    });
+  });
+
+  menuBar.forEach((el) => {
+    el.addEventListener("mouseout", () => {
+      menuBar.forEach((item) => (item.style.color = "#444444"));
+      imgLeft.style.display = "none";
+      imgRight.style.display = "none";
+    });
+  });
+}
+
+///////////////////////////////////////////////////////////////
+
+function setLocalize() {
+  const enElements = document.querySelectorAll(".en");
+  const ruElements = document.querySelectorAll(".ru");
+  const enBtn = document.querySelector(".en-btn");
+  const ruBtn = document.querySelector(".ru-btn");
+  const close = document.querySelector(".close");
+
+  ruElements.forEach((el) => (el.style.display = "none"));
+  enBtn.addEventListener("click", () => {
+    ruBtn.classList.remove("active");
+    enBtn.classList.add("active");
+    enElements.forEach((el) => (el.style.display = "flex"));
+    ruElements.forEach((el) => (el.style.display = "none"));
+    close.textContent = "Close";
+    enBtn.style.color = "black";
+    ruBtn.style.color = "#33333380";
+  });
+
+  ruBtn.addEventListener("click", () => {
+    enBtn.classList.remove("active");
+    ruBtn.classList.add("active");
+    enElements.forEach((el) => (el.style.display = "none"));
+    ruElements.forEach((el) => (el.style.display = "flex"));
+    close.textContent = "Закрыть";
+    ruBtn.style.color = "black";
+    enBtn.style.color = "#33333380";
+  });
+}
+
+setLocalize();
+openCloseBurger();
+openCloseSearch();
+renderImgInMenu();
---
+++ script/slides.js
@@ -0,0 +1,145 @@
+const slidesField = document.querySelector(".block-slide");
+const total = document.querySelector(".total-slides");
+
+function offsetSlides() {
+  let offset = 0;
+  let slideIndex = 1;
+
+  const slides = document.querySelectorAll(".slide");
+  const slider = document.querySelector(".container");
+  const prev = document.querySelector("#prev");
+  const next = document.querySelector("#next");
+  const current = document.querySelector(".current-slide");
+  const slidesWrapper = document.querySelector(".slider-wrapper");
+  const width = window.getComputedStyle(slidesWrapper).width;
+  const indicators = document.querySelector(".current-progress");
+
+  if (slides.length < 10) {
+    total.textContent = `0${slides.length}`;
+    current.textContent = `0${slideIndex}`;
+  } else {
+    total.textContent = slides.length;
+    current.textContent = slideIndex;
+  }
+
+  slidesField.style.width = 100 * slides.length + "%";
+  slidesField.style.display = "flex";
+  slidesWrapper.style.overflow = "hidden";
+
+  slider.style.position = "relative";
+
+  next.addEventListener("click", () => {
+    if (offset == deleteNotDigits(width) * (slides.length - 1)) {
+      offset = 0;
+    } else {
+      offset += deleteNotDigits(width);
+    }
+
+    slidesField.style.transform = `translateX(-${offset}px)`;
+
+    if (slideIndex == slides.length) {
+      slideIndex = 1;
+    } else {
+      slideIndex++;
+    }
+
+    if (slides.length < 10) {
+      current.textContent = `0${slideIndex}`;
+    } else {
+      current.textContent = slideIndex;
+    }
+
+    slidesField.style.transition = "0.5s all";
+    indicators.style.width = `${current.textContent * 20}%`;
+    document.body.style.backgroundImage = `url('../slide/img/slide_${+current.textContent}.png')`;
+  });
+
+  prev.addEventListener("click", () => {
+    if (offset == 0) {
+      offset = deleteNotDigits(width) * (slides.length - 1);
+    } else {
+      offset -= deleteNotDigits(width);
+    }
+
+    slidesField.style.transform = `translateX(-${offset}px)`;
+
+    if (slideIndex == 1) {
+      slideIndex = slides.length;
+    } else {
+      slideIndex--;
+    }
+
+    if (slides.length < 10) {
+      current.textContent = `0${slideIndex}`;
+    } else {
+      current.textContent = slideIndex;
+    }
+
+    slidesField.style.transition = "0.5s all";
+    indicators.style.width = `${current.textContent * 20}%`;
+    document.body.style.backgroundImage = `url('../slide/img/slide_${+current.textContent}.png')`;
+  });
+
+  function deleteNotDigits(str) {
+    return +str.replace(/\D/g, "");
+  }
+}
+
+function renderSlide() {
+  const locales = {
+    en: ["Harmony", "Testing", "Success", "Ideas", "Fate"],
+    ru: ["Гармония", "Тестирование", "Успех", "Идеи", "Судьба"]
+  };
+  locales.en.forEach((el, i) => {
+    const slide = document.createElement("div");
+    slide.classList.add("slide");
+
+    slide.innerHTML = `
+      <h2 class="en">${el}</h2>
+      <h2 class="ru">${locales.ru[i]}</h2>
+        <div class="block-text">
+          <div>
+            <h4 class="en">Concept studio</h4>
+              <p class="en">
+                In our best design works, the Customer will find interiors in various styles, from strict modern minimalism
+                to luxurious, richly decorated, from inexpensive, budgetary solutions to elite and unique.
+              </p>
+            <h4 class="ru">Концепция студии</h4>
+              <p class="ru">
+                В наших лучших дизайнерских работах клиент найдет интерьеры в разных стилях, от строгого модернизма 
+                до роскошных, богато оформленных, от недорогих и бюджетных решений до элитных и уникальных.
+              </p>
+          </div>
+          <div>
+            <h4 class="en">Everyday i’m </h4>
+              <p class="en">
+                In our best design works, the Customer will find interiors in various styles, from strict modern minimalism
+                to luxurious, richly decorated, from inexpensive, budgetary solutions to elite and unique.
+              </p>
+            <h4 class="ru">Каждый день я </h4>
+              <p class="ru">
+                В наших лучших дизайнерских работах клиент найдет интерьеры в разных стилях, от строгого модернизма 
+                до роскошных, богато оформленных, от недорогих и бюджетных решений до элитных и уникальных.
+              </p>
+          </div>
+        </div>
+        <div class="block-btn">
+          <button class="en"><div><span>Read More</span></div></button>
+          <button class="ru"><div><span>Читать далее</span></div></button>
+          <a class="block-link-project">
+            <svg width="19" height="19">
+              <polyline points="0,0 16,7 0,17 0,0" style="fill:none;stroke:rgb(0, 0, 0);stroke-width:1">
+              </polyline>
+            </svg>
+            <div class="triangle"></div>
+            <span class="en">View project</span>
+            <span class="ru">Посмотреть проект</span>
+          </a>
+        </div>          
+      `;
+    slidesField.append(slide);
+  });
+}
+
+renderSlide();
+offsetSlides();
